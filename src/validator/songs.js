ea935bece31192240cf05ef8c32720e6b8904c4c
doyotteee <doyotteee@gmail.com>
2025-06-16 18:55:35 +0700

    update

--- src/validator/songs.js
+++ src/validator/songs.js
@@ -5,14 +5,12 @@ const SongPayloadSchema = Joi.object({
   title: Joi.string().pattern(/^(?!\s*$).+/).required().messages({
     'string.pattern.base': 'Title cannot be empty or only whitespace',
   }),
-  year: Joi.number().integer().min(1900).max(2100).required(),
-  performer: Joi.string().pattern(/^(?!\s*$).+/).required().messages({
+  year: Joi.number().integer().min(1900).max(2100).required(),  performer: Joi.string().pattern(/^(?!\s*$).+/).required().messages({
     'string.pattern.base': 'Performer cannot be empty or only whitespace',
   }),
-  genre: Joi.string().allow(null, '').optional(),
-  duration: Joi.alternatives().try(
+  genre: Joi.string().required(),  duration: Joi.alternatives().try(
     Joi.number().integer().min(1),
-    Joi.valid(null)
+    Joi.valid(null),
   ).optional(),
   albumId: Joi.string().allow(null, '').optional(),
 }).unknown(false);
